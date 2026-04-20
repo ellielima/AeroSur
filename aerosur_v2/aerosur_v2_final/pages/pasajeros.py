e7929614b89fe5aa3681fe3a6a925afe3d9e372c
ellielima <168251510+ellielima@users.noreply.github.com>
2026-04-19 22:45:28 -0600

    Update pasajeros.py

--- aerosur_v2/aerosur_v2_final/pages/pasajeros.py
+++ aerosur_v2/aerosur_v2_final/pages/pasajeros.py
@@ -64,7 +64,7 @@ def render():
             if largo < 9:
                 st.warning(f"⚠️ El pasaporte debe tener exactamente 9 caracteres. Llevas {largo}/9.")
             elif largo ==9:
-                st.success(✅ Longitud de pasaporte correcta (9/9).")
+                st.success("✅ Longitud de pasaporte correcta (9/9).")
             else:
                 st.success("✅ Longitud de pasaporte correcta (9/9).")
 
